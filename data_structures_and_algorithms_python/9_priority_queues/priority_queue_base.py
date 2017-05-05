783447ef1f5bfe582699a1c1ce991ed5919b993d
Alonso <alonsougc@gmail.com>
2017-05-04 21:58:43 -0400

    Book completed ch8 trees and ch9 pq. Code added trees abstract base classes

---
+++ data_structures_and_algorithms_python/9_priority_queues/priority_queue_base.py
@@ -0,0 +1,23 @@
+"""
+
+Priority Queue base class from book
+
+"""
+
+
+class PriorityQueueBase:
+    """ """
+
+    class _Item:
+        """ Lightweight composite to store PQ items """
+        __slots__ = '_key', '_value'
+
+        def __init__(self, k, v):
+            self._key = k
+            self._value = v
+
+        def __lt__(self, other):
+            return self._key < other._key   # true if less than other
+
+    def is_empty(self):     # concrete method assuming abstract len
+        return len(self) == 0
